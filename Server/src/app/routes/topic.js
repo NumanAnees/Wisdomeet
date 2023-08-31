77173b6e101b4e90962bc5f1f2bf649f764a44a6
NumanAnees <numananees44@gmail.com>
2023-08-31 15:31:09 +0500

    minor fixes

--- Server/src/app/routes/topic.js
+++ Server/src/app/routes/topic.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
 //impoort middlewares
-const {
-  requireSignin,
-  authMiddleware,
-  adminMiddleware,
-} = require("../helpers/auth");
+const { requireSignin, authMiddleware } = require("../helpers/auth");
 //import controllers
 const {
   create,
@@ -13,12 +9,16 @@ const {
   deleteTopic,
   followOrUnfollowTopic,
   getFollowCount,
+  getAllTopics,
+  getTopic,
 } = require("../controllers/topic");
 // import validator
 router.post("/", requireSignin, authMiddleware, create);
 router.put("/:id", requireSignin, authMiddleware, update);
 router.delete("/:id", requireSignin, authMiddleware, deleteTopic);
 router.put("/:id/follow", requireSignin, authMiddleware, followOrUnfollowTopic);
-router.get("/:id", getFollowCount);
+router.get("/:id/followers", getFollowCount);
+router.get("/", getAllTopics);
+router.get("/:id", getTopic);
 
 module.exports = router;
