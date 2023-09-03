a6f1d45da9a886bb88015d44e4f6da05bfc9bfa1
NumanAnees <140714988+numananees01@users.noreply.github.com>
2023-09-03 05:32:44 +0500

    answer validation added

--- Server/src/app/routes/answer.js
+++ Server/src/app/routes/answer.js
@@ -10,10 +10,25 @@ const {
   LikeDislikeAnswer,
 } = require("../controllers/answer");
 // import validator
-
+const { answerValidator } = require("../validators/answer");
+const { runValidation } = require("../validators");
 //Routes
-router.post("/:id", requireSignin, authMiddleware, createAnswer);
-router.put("/:id", requireSignin, authMiddleware, updateAnswer);
+router.post(
+  "/:id",
+  requireSignin,
+  authMiddleware,
+  answerValidator,
+  runValidation,
+  createAnswer
+);
+router.put(
+  "/:id",
+  requireSignin,
+  authMiddleware,
+  answerValidator,
+  runValidation,
+  updateAnswer
+);
 router.delete("/:id", requireSignin, authMiddleware, deleteAnswer);
 router.post("/:id/like", requireSignin, authMiddleware, LikeDislikeAnswer);
 
