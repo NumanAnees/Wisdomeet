df7a3bcaefcc7a54a5e00ef6125037272d692c1a
NumanAnees <numananees44@gmail.com>
2023-09-04 15:00:48 +0500

    signup form added

---
+++ Client/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import PrivateRoute from "./helpers/PrivateRoute";
+import Error from "./components/Error";
+
+import "react-toastify/dist/ReactToastify.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+export const ThemeContext = React.createContext();
+
+const App = () => {
+  const [currentUser, setCurrentUser] = useState();
+  //simple useEffect...
+  useEffect(() => {
+    SetUser();
+  }, []);
+  //set user function for setting current user...
+  const SetUser = () => {
+    const userId = localStorage.getItem("currentUser");
+    const existingUsers = JSON.parse(localStorage.getItem("users")) || [];
+
+    const user = existingUsers.find((el) => el.id === userId);
+    setCurrentUser(user);
+  };
+  return (
+    <>
+      <ToastContainer />
+      <ThemeContext.Provider value={currentUser}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<PrivateRoute />}>
+              <Route path="/" element={<Home />} exact />
+            </Route>
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/signup" exact element={<Signup />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeContext.Provider>
+    </>
+  );
+};
+
+export default App;
