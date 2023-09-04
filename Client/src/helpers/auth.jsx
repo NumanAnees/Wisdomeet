bd138a9a8d0d1ea3735be2c72d5547111069e3ab
NumanAnees <140714988+numananees01@users.noreply.github.com>
2023-09-04 19:31:57 +0500

    Update profile added

--- Client/src/helpers/auth.jsx
+++ Client/src/helpers/auth.jsx
@@ -1,3 +1,5 @@
+import Cookies from "js-cookie";
+
 export const isAuth = () => {
   const currentUser = localStorage.getItem("currentUser");
   return !!currentUser; // Returns true if currentUser exists, false otherwise
@@ -6,4 +8,10 @@ export const isAuth = () => {
 export const getUser = () => {
   const currentUser = localStorage.getItem("currentUser");
   return JSON.parse(currentUser);
+};
+
+export const getToken = () => {
+  const token = Cookies.get("token");
+  console.log(token);
+  return token;
 };
