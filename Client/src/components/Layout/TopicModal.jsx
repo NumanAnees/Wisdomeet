15ccce41031fc2fb5e48f3091d96932f66d602a7
NumanAnees <140714988+numananees01@users.noreply.github.com>
2023-09-05 13:56:50 +0500

    Add topic implemented

---
+++ Client/src/components/Layout/TopicModal.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import { Modal } from "antd";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import { getToken } from "../../helpers/auth";
+import { toast } from "react-toastify";
+
+const TopicModalComponent = () => {
+  const [open, setOpen] = useState(false);
+  //validation...
+  const validationSchema = Yup.object().shape({
+    title: Yup.string()
+      .min(3, "title must be at least 3 characters long")
+      .required("Title is required"),
+    description: Yup.string()
+      .min(3, "description must be at least 3 characters long")
+      .required("Description is required"),
+  });
+
+  const handleSubmit = async (values, { resetForm }) => {
+    alert("Submit");
+    const authToken = getToken();
+    try {
+      const formData = new FormData();
+      formData.append("title", values.title);
+      formData.append("description", values.description);
+      formData.append("picture", values.picture);
+
+      const response = await axios.post(
+        "http://localhost:8000/api/topics",
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      if (response.status === 201 || response.status === 200) {
+        resetForm();
+        setOpen(false);
+        toast.success("Topic added successfully!");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Failed to add topic");
+    }
+  };
+
+  return (
+    <>
+      <button className="nav-btn" onClick={() => setOpen(true)}>
+        Add Topic
+      </button>
+
+      <Modal
+        title="Add new Topic"
+        centered
+        open={open}
+        footer={null}
+        className="custom-modal"
+        width={1300}
+        onCancel={() => setOpen(false)}
+      >
+        <Formik
+          initialValues={{
+            title: "",
+            description: "",
+            picture: null,
+          }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {(
+            { setFieldValue } // Use setFieldValue to handle file input changes manually
+          ) => (
+            <Form>
+              <div className="mb-3">
+                <Field
+                  type="text"
+                  name="title"
+                  className="form-control"
+                  placeholder="Title"
+                />
+                <ErrorMessage
+                  name="title"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+              <div className="mb-3">
+                <Field
+                  type="description"
+                  name="description"
+                  className="form-control"
+                  placeholder="Description"
+                />
+                <ErrorMessage
+                  name="description"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+              <div className="mb-3">
+                <input
+                  type="file"
+                  name="picture"
+                  className="form-control"
+                  onChange={(event) => {
+                    setFieldValue("picture", event.currentTarget.files[0]); //imp...
+                  }}
+                />
+                <ErrorMessage
+                  name="picture"
+                  component="div"
+                  className="text-danger"
+                />
+              </div>
+              <button type="submit" className="btn btn-success w-100">
+                Add Topic
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </Modal>
+    </>
+  );
+};
+
+export default TopicModalComponent;
