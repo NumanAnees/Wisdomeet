749eb4d7ca02139374edb209a6df7c77cb3f9f17
NumanAnees <numananees44@gmail.com>
2023-11-07 06:45:46 +0500

    blank screen fixed

---
+++ Server/src/app/helpers/constants.js
@@ -0,0 +1,8 @@
+module.exports.HTTP_STATUS_OK = 200;
+module.exports.HTTP_STATUS_CREATED = 201;
+module.exports.HTTP_STATUS_BAD_REQUEST = 400;
+module.exports.HTTP_STATUS_UNAUTHORIZED = 401;
+module.exports.HTTP_STATUS_NOT_ALLOWED = 403;
+module.exports.HTTP_STATUS_FORBIDDEN = 203;
+module.exports.HTTP_STATUS_NOT_FOUND = 404;
+module.exports.HTTP_STATUS_SERVER_ERROR = 500;
