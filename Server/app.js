8da3ab2217271ba2082d13ddaef6f68f7b0bfddd
NumanAnees <numananees44@gmail.com>
2023-11-07 06:45:45 +0500

    Auth completed

--- Server/app.js
+++ Server/app.js
@@ -3,100 +3,25 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 require("dotenv").config();
 const sequelize = require("./src/db/db");
-const { DataTypes } = require("sequelize");
-const User = require("./src/models/user")(sequelize, DataTypes);
 const passport = require("passport");
-require("./config/passport");
-const jwt = require("jsonwebtoken");
-const session = require("express-session");
+require("./src/config/passport");
+const fileUpload = require("express-fileupload");
 
 //Initializing express
 const app = express();
 
-//Initializing
 //Express Middleware
 app.use(bodyParser.json());
 app.use(morgan("dev"));
-app.use(
-  session({
-    secret: "ABADADADDABADVADVDAeq42g2222",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
 app.use(passport.initialize());
-app.use(passport.session());
+app.use(fileUpload({ useTempFiles: true }));
 
-//Login Route
-// Login route
-// Login route
-app.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, user) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.status(401).json({ message: "Incorrect email or password." });
-    }
+//routes
+const authRoutes = require("./src/app/routes/user");
+//middlewares
+app.use("/api", authRoutes);
 
-    // Generate JWT token
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-
-    // Return user object and token
-    res.json({ user: user, token: token });
-  })(req, res, next);
-});
-
-//Logout Route
-app.get("/logout", (req, res) => {
-  req.logout();
-  res.send({ message: "Logged out" });
-});
-// Registration route
-app.post("/register", async (req, res) => {
-  try {
-    const { email, password, name, age, gender } = req.body;
-
-    // Check if the email is already registered
-    const existingUser = await User.findOne({ where: { email: email } });
-    if (existingUser) {
-      return res.status(409).json({ message: "Email is already registered." });
-    }
-
-    // Create a new user
-    const newUser = await User.create({
-      email: email,
-      password: password,
-      name: name,
-      age: age,
-      gender: gender,
-    });
-
-    // Generate JWT token
-    const token = jwt.sign({ userId: newUser.id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-
-    // Return user object and token
-    res.json({ user: newUser, token: token });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Registration failed." });
-  }
-});
-
-// const isAuthenticated = (req, res, next) => {
-//   if (req.user) return next();
-//   else
-//     return res.status(401).json({
-//       error: "User not authenticated",
-//     });
-// };
-
-// app.use(isAuthenticated);
-//Route
+//Testing route
 app.get("/", (req, res) => {
   res.send({ message: "Hello World" });
 });
--- Server/src/app/models/user.js
+++ Server/src/app/models/user.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../db/db");
+const sequelize = require("../../db/db");
 const bcrypt = require("bcrypt");
 
 module.exports = function (sequelize, Sequelize) {
