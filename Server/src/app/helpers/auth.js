e691ad88233932e854ce18027d9b6cb289b017f8
NumanAnees <numananees44@gmail.com>
2023-11-07 06:45:45 +0500

    user update added

---
+++ Server/src/app/helpers/auth.js
@@ -0,0 +1,23 @@
+const expressJwt = require("express-jwt");
+const sequelize = require("../../db/db");
+const { DataTypes } = require("sequelize");
+const User = require("../models/user")(sequelize, DataTypes);
+
+//--------------------------------------Authorization--------------------------------
+exports.requireSignin = expressJwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ["sha1", "RS256", "HS256"],
+}); // req.user
+
+exports.authMiddleware = async (req, res, next) => {
+  const authUserId = req.user.id;
+  console.log(req.user);
+  const user = await User.findOne({ where: { id: authUserId } });
+  if (!user) {
+    return res.status(400).json({
+      error: "User not found",
+    });
+  }
+  req.profile = user;
+  next();
+};
--- Server/src/app/routes/user.js
+++ Server/src/app/routes/user.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
+//middlewares
+const { requireSignin, authMiddleware } = require("../helpers/auth");
 //controllers
-const { register, login } = require("../controllers/user");
+const { register, login, update } = require("../controllers/user");
 
 router.post("/register", register);
 router.post("/login", login);
+router.put("/update", requireSignin, authMiddleware, update);
 
 module.exports = router;
